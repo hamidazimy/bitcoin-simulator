1892f9cb2ea5b9fb94e8d7f32a0f015760af036c
Hamid Azimy <hamid.azimy@gmail.com>
2019-10-31 09:27:35 -0300

    Add 3D plot.

--- semiselfishformula.py
+++ semiselfishformula.py
@@ -1,5 +1,7 @@
 import numpy as np
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
+from matplotlib import rc
 
 
 def r_selfish(a, g):
@@ -9,6 +11,7 @@ def r_semi_selfish(a, g):
     return (a * (a * (a * (2 * a - 5) + 4) - (a - 1) ** 3 * g)) / ((a - 1) * a ** 2 + 1)
 
 
+"""
 plt.figure(figsize=(12, 4))
 
 alpha = np.linspace(0, 0.5, 501)
@@ -41,5 +44,30 @@ plt.xlim(0, 0.5)
 plt.ylim(0, 1)
 plt.legend()
 plt.show()
+"""
 
+fig = plt.figure()
+ax = fig.add_subplot(121, projection='3d')
+ax2 = fig.add_subplot(122, projection='3d')
+
+G = np.arange(0, 1.0, 0.1)
+len_G = np.shape(G)[0]
+A = np.arange(0, 0.5, .02)
+len_A = np.shape(A)[0]
+
+X, Y = np.meshgrid(G, A)
+
+SS = np.ones((len_A, len_G))
+S = np.ones((len_A, len_G))
+
+for i in range(len_A):
+    for j in range(len_G):
+        S[i, j] = r_selfish(A[i], G[j])
+        SS[i, j] = r_semi_selfish(A[i], G[j])
+
+
+ax.plot_surface(X, Y, S, rstride=1, cstride=1, cmap='plasma', edgecolor='none')
+ax.plot_surface(X, Y, SS, rstride=1, cstride=1, cmap='plasma', edgecolor='none')
+
+plt.show()
 
